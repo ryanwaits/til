b29846ea652981e1e5eca2776523a6771d46d79a
Ryan Waits <ryan.waits@gmail.com>
2016-03-20 16:11:42 -0500

    add post routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   root 'home#index'
+  resources :posts, except: [:index]
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -8,7 +10,6 @@ Rails.application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
