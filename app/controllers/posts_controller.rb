ca374da1c80e16ca2c00e53eb135d162a8cf90e4
Ryan Waits <ryan.waits@gmail.com>
2016-03-20 16:14:03 -0500

    add post controller

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,35 @@
+class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit]
+  def new
+    @post = Post.new
+  end
+
+  def create
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, extensions = {})
+    @post = Post.new(post_params)
+    @post.body = markdown.render(@post.body)
+    @post.author_id = 1
+    
+    @post.save ? (redirect_to(root_path)) : (render 'new')
+    
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  private
+
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :body, :hashtag)
+  end
+end
