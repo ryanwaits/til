3ab649ef2d76b0a94139f862740ced1a32f2e1bb
Ryan Waits <ryan.waits@gmail.com>
2016-03-21 01:19:17 -0500

    add flash notice for sessions // add minor design changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,14 +7,17 @@ class SessionsController < ApplicationController
     author = Author.find_by_email(params[:session][:email])
     if author && author.authenticate(params[:session][:password])
         session[:author_id] = author.id
+        flash[:logged_in] = "Welcome back #{author.username}!"
         redirect_to root_path
     else
+      flash[:logged_in] = "Your email or password was incorrect, try again."
         redirect_to login_path
     end
   end
 
   def destroy
     session[:author_id] = nil
+    flash[:logged_out] = "You successfully logged out."
     redirect_to login_path
   end
 end
