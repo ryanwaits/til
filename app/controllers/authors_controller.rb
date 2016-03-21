95da63005b45f0c83e5cd78345b8f7cbadbba9ed
Ryan Waits <ryan.waits@gmail.com>
2016-03-21 17:31:53 -0500

    log user in when they sign up // add more authentication

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,6 @@
 class AuthorsController < ApplicationController
     before_action :set_author, only: [:edit, :show]
+    include AuthorsHelper
   def new
     @author = Author.new
   end
@@ -8,8 +9,8 @@ class AuthorsController < ApplicationController
     @author = Author.create(author_params)
 
     if @author.save
-      flash[:signup] = "Thanks for signing up <% @author.username %>"
-      redirect_to root_path
+      login(@author)
+      flash[:signup] = "Thanks for signing up!"
     else
       flash[:signup] = "Try again"
       render 'new'
