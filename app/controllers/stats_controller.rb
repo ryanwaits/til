0855a59f11bbdf996a63d9e3f73c8f39c5b1608c
Ryan Waits <ryan.waits@gmail.com>
2016-03-21 18:46:00 -0500

    modify helper methods

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -4,7 +4,8 @@ class StatsController < ApplicationController
   def index
     @author = session[:author_id]
     if @author
-        build_graph(session[:author_id])
+        build_posts_graph(session[:author_id])
+        build_hashtags_graph
     else
         flash[:access] = "You must be logged in to see your stats."
         redirect_to login_path
