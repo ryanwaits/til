0ea5a2461c0b4a6f2bb4eac81f6b8902ad944d19
Ryan Waits <ryan.waits@gmail.com>
2016-03-21 17:32:50 -0500

    add login method

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -2,4 +2,9 @@ module AuthorsHelper
     def logged_in?(author)
         author ? true : false
     end
+
+    def login(author)
+        session[:author_id] = author.id
+        redirect_to root_path
+    end
 end
