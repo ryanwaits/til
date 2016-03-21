3f7aebc2a629b2ae3360bee483b62d3667edf933
Ryan Waits <ryan.waits@gmail.com>
2016-03-21 12:34:46 -0500

    create likes table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160320022138) do
+ActiveRecord::Schema.define(version: 20160321162159) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "username"
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20160320022138) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "author_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "likes", ["author_id"], name: "index_likes_on_author_id"
+  add_index "likes", ["post_id"], name: "index_likes_on_post_id"
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
