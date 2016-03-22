6261edff753acae325faf509d69c556e86ab8b3d
Ryan Waits <ryan.waits@gmail.com>
2016-03-22 12:18:30 -0500

    add helper method to prevent authors liking their own posts

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,16 +1,24 @@
 class LikesController < ApplicationController
+  include LikesHelper
   def new
     
   end
 
   def create
-    @like = Like.create(author_id: session[:author_id], post_id: params[:post_id])
+    if not_authorized?
+      flash[:like] = "You can't like your own posts, nice try though."
+      redirect_to root_path
 
-    if @like.save
-        redirect_to root_path
     else
-        flash[:like] = "You are not authorized to like posts yet, please sign in."
-        redirect_to root_path
+      @author = session[:author_id]
+      like = Like.create(author_id: @author, post_id: params[:post_id])
+
+      if like.save
+          redirect_to root_path
+      else
+          flash[:like] = "You are not authorized to like posts yet, please sign in."
+          redirect_to root_path
+      end
     end
   end
 end
--- app/helpers/likes_helper.rb
+++ app/helpers/likes_helper.rb
@@ -1,2 +1,13 @@
 module LikesHelper
+    def not_authorized?
+        author = session[:author_id]
+        my_posts = Author.find(author).posts
+        my_posts_ids = []
+        my_posts.each do |post|
+            my_posts_ids.push(post.id)
+        end
+
+        return my_posts_ids.include?(params[:post_id].to_i)
+
+    end
 end
