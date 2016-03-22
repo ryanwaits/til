65de799a772fe81b4a0887e6013f4369277b8552
Ryan Waits <ryan.waits@gmail.com>
2016-03-21 21:20:26 -0500

    add more graph helpers for all posts likes

--- app/helpers/stats_helper.rb
+++ app/helpers/stats_helper.rb
@@ -1,5 +1,34 @@
 module StatsHelper
 
+    def build_all_posts_graph
+        posts = []
+        all_posts = Post.all
+        all_posts.each do |post|
+            posts.push({ post.title.truncate(25) => post.likes.count })
+        end
+        labels = []
+        data = []
+        posts.each { |post| labels.push(post.keys) }
+        posts.each { |post| data.push(post.values) }
+
+        @data = {
+            labels: labels,
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(246, 53, 51, 0.7)",
+                    strokeColor: "rgba(246, 53, 51, 0.9)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: data
+                }
+            ]
+        }
+        @options = {}
+    end
+
     def build_posts_graph(author)
         if author
             likes = []
@@ -40,6 +69,33 @@ module StatsHelper
         end
         hashtags.each { |hash| counts[hash] += 1 }
         
+        @data = {
+            labels: counts.keys,
+            datasets: [
+                {
+                    label: "My First dataset",
+                    fillColor: "rgba(246, 53, 51, 0.7)",
+                    strokeColor: "rgba(246, 53, 51, 0.9)",
+                    pointColor: "rgba(220,220,220,1)",
+                    pointStrokeColor: "#fff",
+                    pointHighlightFill: "#fff",
+                    pointHighlightStroke: "rgba(220,220,220,1)",
+                    data: counts.values
+                }
+            ]
+        }
+        @options = {}
+    end
+
+    def build_posts_per_day_graph
+        counts = Hash.new(0)
+        days = []
+        @posts = Post.all
+        @posts.each do |post|
+            days.push(post.created_at.strftime("%B %d, %Y"))
+        end
+        days.each { |day| counts[day] += 1 }
+
         @data = {
             labels: counts.keys,
             datasets: [
